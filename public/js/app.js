0566452934ecb350c3a50689b44d7f96a4ae34d7
Chroslen <chroslen@gmail.com>
2017-02-14 02:19:55 +0800

    signup signin title

--- public/js/app.js
+++ public/js/app.js
@@ -5,22 +5,27 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function config($locationProvider, $routeProvider) {
     $routeProvider.
     when('/', {
+        title: 'Home | ',
         templateUrl: 'partials/index',
         controller: IndexCtrl
     }).
     when('/signup', {
+        title: 'Signup | ',
         templateUrl: 'partials/signup',
         controller: SignupCtrl
     }).
     when('/signin', {
+        title: 'Signin | ',
         templateUrl: 'partials/signin',
         controller: SigninCtrl
     }).
     when('/myprofile', {
+        title: 'My profile | ',
         templateUrl: 'partials/myprofile',
         controller: MyprofileCtrl
     }).
     when('/signout', {
+        title: 'Signout | ',
         templateUrl: 'partials/index',
         controller: SignoutCtrl
     }).
