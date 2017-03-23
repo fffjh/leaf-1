997e27588d2078a9f90b548eef4327a737aaf7d9
Chroslen <chroslen@gmail.com>
2017-03-23 20:47:51 +0800

    drag on phone

--- public/js/dragBar.js
+++ public/js/dragBar.js
@@ -1,23 +1,15 @@
 var isResizing = false,
-    lastDownX = 0,
-    lastDownY = 0;
+    lastDownX = 0;
 
 $(function() {
     var container = $('#container'),
         left = $('#left_panel'),
         right = $('#right_panel'),
-        top = $('#top_panel'),
-        bottom = $('#bottom_panel'),
-        dragVertical = $('#drag-vertical'),
-        dragHorizontal = $('#drag-horizontal');
+        drag = $('#drag');
 
-    dragVertical.on('mousedown', function(e) {
+    drag.on('mousedown', function(e) {
         isResizing = true;
         lastDownX = e.clientX;
-    });
-    dragHorizontal.on('mousedown', function(e) {
-        isResizing = true;
-        lastDownY = e.clientY;
     });
 
     $(document).on('mousemove', function(e) {
@@ -26,12 +18,9 @@ $(function() {
             return;
 
         var offsetRight = container.width() - (e.clientX - container.offset().left);
-        var offsetBottom = $(window).height() - (e.clientY - container.offset().top);
 
         left.css('right', offsetRight);
         right.css('width', offsetRight);
-        top.css('bottom', offsetBottom);
-        bottom.css('height', offsetBottom);
     }).on('mouseup', function(e) {
         // stop resizing
         isResizing = false;
